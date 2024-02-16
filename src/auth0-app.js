870c9c05a316ec6816380551f8906bb06afcb9df
niedag <grantnieda@gmail.com>
2024-02-16 15:53:41 -0800

    first step in tutorial

---
+++ src/auth0-app.js
@@ -0,0 +1,16 @@
+import { createAuth0 } from '@auth0/auth0-vue';
+
+const app = createApp(App);
+
+app.use(
+    createAuth0({{
+        domain: "dev-y5agcwlppf7dyjjb.us.auth0.com",
+        clientId: "Hha3WGDqamPQ50ULrtEIntjEr84TaaGW",
+        authorizationParams: {
+            redirect_uri:  window.location.origin
+        }
+    })
+  })
+);
+
+app.mount('#app');
